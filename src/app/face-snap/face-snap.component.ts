1268382504015c00c3786de92f29887f620faf0e
Leopold <leopolddag@gmail.com>
2024-01-15 11:40:57 +0100

    first commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = "archibald";
+    this.description = "Mon meilleur ami depuis tout petit";
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';  }
+}
+
